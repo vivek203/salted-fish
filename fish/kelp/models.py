10cce36713826bdbe160a0cf656efc330ff17357
Yunduz <yunduz@gmail.com>
2016-04-23 08:20:56 -0700

    added new tables

--- fish/kelp/models.py
+++ fish/kelp/models.py
@@ -8,8 +8,17 @@ class WaterTemperature(models.Model):
     timestamp = models.DateTimeField()
     value = models.FloatField()
 
+
 class DischargeData(models.Model):
     station_id = models.CharField(max_length=100)
     station_name = models.CharField(max_length=200)
     timestamp = models.DateTimeField()
     value = models.FloatField()
+    velocity = models.FloatField(default=0)
+
+class SpawningStreamLength(models.Model):
+    incubation_time = models.FloatField()
+    stream_length = models.FloatField()
+    actual_length = models.FloatField()
+    station_id = models.CharField(max_length=100)
+    station_name = models.CharField(max_length=200)
