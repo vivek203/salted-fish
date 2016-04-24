481476abb19cb1d2db2f2f9ff9ec6593364a6bff
khanyawar <khan.yawar@gmail.com>
2016-04-24 02:18:41 -0700

    updated urls

--- fish/kelp/urls.py
+++ fish/kelp/urls.py
@@ -8,3 +8,4 @@ urlpatterns = [
      url(r'^populatewtc/$', views.populate_curr_water_temperature, name='populate_wtc'),
      url(r'^populatedch/$', views.populate_full_discharge, name='populate_dch'),
      url(r'^calc_gdd/$', views.calc_gdd, name='calc_gdd'),
+]
